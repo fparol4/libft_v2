188ec1ea47960562ef2c941b6619cd6571239405
Fabricio Parola <fabricio.parola@gmail.com>
2025-11-05 20:32:14 -0300

    chore: printf #1

--- lib/ft_printf/ft_handler.c
+++ lib/ft_printf/ft_handler.c
@@ -12,45 +12,40 @@
 
 #include "../../libft.h"
 
-int	ft_handle_int(int value)
-{
-	char	*v_str;
-	char	*b_dec;
+int ft_handle_int(int value) {
+  char *v_str;
+  char *b_dec;
 
-	b_dec = "0123456789";
-	v_str = ft_itoab(value, b_dec);
-	return (ft_putstr_fd(v_str, 1));
+  b_dec = "0123456789";
+  v_str = ft_itoab(value, b_dec);
+  return (ft_putstr_fd(v_str, 1));
 }
 
-int	ft_handle_unsigned(unsigned int value)
-{
-	char	*v_str;
-	char	*b_dec;
+int ft_handle_unsigned(unsigned int value) {
+  char *v_str;
+  char *b_dec;
 
-	b_dec = "0123456789";
-	v_str = ft_itoa_unsigned(value, b_dec);
-	return (ft_putstr(v_str, 1));
+  b_dec = "0123456789";
+  v_str = ft_itoab_uns(value, b_dec);
+  return (ft_putstr_fd(v_str, 1));
 }
 
-int	ft_handle_hex(unsigned int v, int upper)
-{
-	char	*b_hex;
-	char	*v_str;
+int ft_handle_hex(unsigned int v, int upper) {
+  char *b_hex;
+  char *v_str;
 
-	b_hex = "0123456789abcdef";
-	if (upper)
-		b_hex = "0123456789ABCDEF";
-	v_str = ft_itoa_unsigned(v, b_hex);
-	return (ft_putstr(v_str, 1));
+  b_hex = "0123456789abcdef";
+  if (upper)
+    b_hex = "0123456789ABCDEF";
+  v_str = ft_itoab_uns(v, b_hex);
+  return (ft_putstr_fd(v_str, 1));
 }
 
-int	ft_handle_ptr(size_t address)
-{
-	char	*b_hex;
-	char	*v_str;
+int ft_handle_ptr(size_t address) {
+  char *b_hex;
+  char *v_str;
 
-	b_hex = "0123456789abcdef";
-	v_str = ft_itoa_unsigned((size_t) address, b_hex);
-	ft_putstr("0x", 0);
-	return (ft_putstr(v_str, 1) + 2);
+  b_hex = "0123456789abcdef";
+  v_str = ft_itoab_uns((size_t)address, b_hex);
+  return (ft_putstr_fd("0x", 0) + ft_putstr_fd(v_str, 1));
 }
